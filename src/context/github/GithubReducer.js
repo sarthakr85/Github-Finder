2edb29cb3ba7e1b33ddcc96e3592b4b25221456e
Sarthak Rathi <sarthakr85@gmail.com>
2022-12-29 12:58:29 +0530

    done with github API part

---
+++ src/context/github/GithubReducer.js
@@ -0,0 +1,20 @@
+const githubReducer = function (state, action) {
+  switch (action.type) {
+    case "GET_USERS":
+      return { ...state, users: action.payload, isLoading: false };
+    case "SET_LOADING":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "CLEAR_USERS":
+      return {
+        ...state,
+        users: [],
+      };
+    default:
+      return state;
+  }
+};
+
+export default githubReducer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
+
 function Home() {
   return (
-    <div>
-      <h1 className="text-6xl">Welcome</h1>
-      {process.env.REACT_APP_GITHUB_TOKEN}
-    </div>
+    <>
+      <UserSearch />
+      <UserResults />
+    </>
   );
 }
 export default Home;
