3f575cfdae5b9fd41bd481cd0aafca26ba36da6c
Sarthak Rathi <sarthakr85@gmail.com>
2023-01-03 21:00:13 +0530

    refactored context to actions and uses axios instead of fetch

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -2,18 +2,14 @@ const githubReducer = function (state, action) {
   switch (action.type) {
     case "GET_USERS":
       return { ...state, users: action.payload, isLoading: false };
-    case "GET_USER":
+    case "GET_USER_AND_REPOS":
       return {
         ...state,
-        user: action.payload,
+        user: action.payload.user,
+        repos: action.payload.repos,
         isLoading: false,
       };
-    case "GET_REPOS":
-      return {
-        ...state,
-        repos: action.payload,
-        isLoading: false,
-      };
+
     case "SET_LOADING":
       return {
         ...state,
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,17 +5,22 @@ import Spinner from "../components/layout/Spinner";
 import RepoList from "../components/repos/RepoList";
 import GithubContext from "../context/github/GithubContext";
 import { useParams } from "react-router-dom";
+import { getUserAndRepos } from "../context/github/GithubActions";
 
 function User() {
-  const { getUser, user, isLoading, getUserRepos, repos } =
-    useContext(GithubContext);
+  const { user, isLoading, repos, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
-  }, []);
+    dispatch({ type: "SET_LOADING" });
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login);
+
+      dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
+    };
+    getUserData();
+  }, [dispatch, params.login]);
 
   const {
     name,
